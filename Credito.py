a0c158e849c64909c27f678e6ad5c3d8a98f2bf0
blackyshooter <elias.soto.py@outlook.com>
2024-05-24 10:51:14 -0400

    Credito 4.0

--- Credito.py
+++ Credito.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Credito:
     def __init__(self, monto, plazo, interes):
         self.monto = monto
@@ -6,7 +9,7 @@ class Credito:
 
 
 class CreditoFrances(Credito):
-    def __init__(self, monto, plazo, interes):
+    def __constructfran__(self, monto, plazo, interes):
         super().__init__(monto, plazo, interes)
 
     def amortizacion(self):
@@ -17,28 +20,34 @@ class CreditoFrances(Credito):
 
 
 class CreditoAleman(Credito):
-    def __init__(self, monto, plazo, interes):
+    def __constructale___(self, monto, plazo, interes):
         super().__init__(monto, plazo, interes)
 
     def amortizacion(self):
+        """
+        Calcula la amortización del préstamo utilizando el método de amortización alemán.
+
+        :return: Una lista de las cuotas amortizadas para cada período.
+        :rtype: list[int]
+        """
         cuotas_amortizadas = []
         cuota_capital = self.monto / self.plazo
         saldo = self.monto
         for i in range(self.plazo):
-            interes = saldo * self.interes
+            interes = saldo * self.interes / 12
             cuota_total = int(cuota_capital + interes)
-            cuota_capital -= saldo
             cuotas_amortizadas.append(cuota_total)
+            saldo -= cuota_capital
         return cuotas_amortizadas
 
 
 def main():
-    import math
-    # Crear una instancia de la clase CreditoFrances
+    # Crear una instancia de la clase credito_frances
     mi_creditofran = CreditoFrances(120000000, 60, 0.12)
     # Llamar al método amortizacion e imprimir el resultado
     print(math.ceil(mi_creditofran.amortizacion()))
 
+    # Crear una instancia de la clase credito_aleman
     mi_creditoale = CreditoAleman(120000000, 60, 0.12)
     print(mi_creditoale.amortizacion())
 
